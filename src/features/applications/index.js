b2470a27ded9d37f61d0c08c23241eed9ce80127
Khomurod <46620174+Khomurod@users.noreply.github.com>
2026-01-02 13:22:15 +0000

    new error fix

---
+++ src/features/applications/index.js
@@ -0,0 +1,5 @@
+export { ApplicationsView } from './components/ApplicationsView';
+export { useAppActions } from './hooks/useAppActions';
+export { useAppFetch } from './hooks/useAppFetch';
+export { useApplicationDetails } from './hooks/useApplicationDetails';
+export * from './services/applicationService';
