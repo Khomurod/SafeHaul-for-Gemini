82407343abfadd2e7bd0df511b258250475e81c5
Khomurod <46620174+Khomurod@users.noreply.github.com>
2026-01-04 17:04:30 +0500

    new changes

--- src/features/settings/components/hiring/index.js
+++ src/features/settings/components/hiring/index.js
@@ -1,5 +1,14 @@
-export { PAY_TYPES, EXPERIENCE_LEVELS, FREIGHT_TYPES, BENEFITS_LIST, INITIAL_SUB_POSITION_STATE, INITIAL_HIRING_STATE } from './HiringConfig';
-export { HiringPositionsBuilder } from './HiringPositionsBuilder';
-export { SubPositionEditor } from './SubPositionEditor';
+export { 
+    PAY_TYPES, 
+    EXPERIENCE_LEVELS, 
+    FREIGHT_TYPES, 
+    BENEFITS_LIST, 
+    INITIAL_SUB_POSITION_STATE, 
+    INITIAL_HIRING_STATE 
+} from './HiringConfig';
+
+// REMOVED: HiringPositionsBuilder
+// REMOVED: SubPositionEditor
+
 export { JobWizard } from './JobWizard';
 export { JobPostingManager } from './JobPostingManager';
