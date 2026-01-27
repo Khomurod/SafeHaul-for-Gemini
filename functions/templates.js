dcdef1262fd9c34312a5b23575a4ce36ab87205f
Kholmurod <holmurod96@gmail.com>
2026-01-27 12:40:06 -0600

    deleted all

--- functions/templates.js
+++
@@ -1,9 +0,0 @@
-const functions = require("firebase-functions");
-const { admin, db } = require("./firebaseAdmin");
-
-/**
- * templates.js
- * All CRUD operations for message templates have been refactored
- * to use the direct Firestore SDK on the frontend for performance
- * and cost efficiency.
- */
