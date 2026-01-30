d8ffd6c01dbf5e20f9bd04348c483d19912fac30
Kholmurod <holmurod96@gmail.com>
2026-01-30 09:26:55 -0600

    some ideas applied

---
+++ src/features/company-admin/components/application/index.js
@@ -0,0 +1,4 @@
+export { Section, InfoGrid, InfoItem } from './ApplicationUI';
+export { PersonalInfoSection } from './sections/PersonalInfoSection';
+export { QualificationsSection } from './sections/QualificationsSection';
+export { SupplementalSection } from './sections/SupplementalSection';
